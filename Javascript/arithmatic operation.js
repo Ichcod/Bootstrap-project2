5f415bec8e93def847fe0776a278b68ab4f6066b
Ichcod <ogeamosicha@gmail.com>
2024-07-18 13:34:38 +0100

    push to git

---
+++ Javascript/arithmatic operation.js
@@ -0,0 +1,20 @@
+let variableOne = 20;
+let variableTwo = 50;
+let variableThree = 70;
+let variableFour = 100;
+console.log(variableOne + variableTwo);
+console.log(variableOne - variableTwo);
+console.log(variableTwo / variableOne);
+console.log(variableTwo % variableOne);
+console.log(variableOne < variableTwo)
+console.log(variableOne < variableTwo && variableThree > variableFour)
+console.log(variableOne < variableTwo || variableThree > variableFour)
+
+function Multiplication(){
+    let variableOne = 20;
+    let version = 17
+    let result = variableOne * version
+    console.log(result)
+}
+
+Multiplication()
